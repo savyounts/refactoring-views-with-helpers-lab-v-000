eeee1e445f3dee0fb1324d2e1d80983c2bd55651
Savannah Younts <savyounts@gmail.com>
2018-09-26 15:58:21 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
 
   def display_artist(song)
-    if !song.artist
+    if song.artist
       <%= link_to song.artist_name, artist_path(song.artist) %>
     else
       <%= link_to song.artist_name, artist_path(song.artist) %>
